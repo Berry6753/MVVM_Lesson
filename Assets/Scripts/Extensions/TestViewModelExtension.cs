f0ffe37c66d7cb82d9ce0e16360dc2fc832ab909
WSM <wsm6753@gmail.com>
2024-06-14 16:20:46 +0900

    MVVM

--- Assets/Scripts/Extensions/TestViewModelExtension.cs
+++ Assets/Scripts/Extensions/TestViewModelExtension.cs
@@ -1,16 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TestViewModelExtension : MonoBehaviour
+namespace ViewModel.Extensions
 {
-    void Start()
+    public static class TestViewModelExtension
     {
-        
-    }
+        protected abstract Class class
 
-    void Update()
-    {
-        
+        public static void RefreshViewModel(this  vm)
+        {
+            int tempId = 2;
+            GameLogicManager.Inst.RefreshCharacterInfo(tempId, vm.OnRefreshViewModel);
+        }
+
+        public static void OnRefreshViewModel(this  vm, int userId, string name, int level)
+        {
+            vm.UserId = userId;
+            vm.Name = name;
+            vm.Level = level;
+        }
+
+        public static void RegisterEventsOnEnable(this  vm)
+        {
+            GameLogicManager.Inst.RegisterLevelUpCallback(vm.OnResponseLevelUp);
+            GameLogicManager.Inst.RegisterNameChangeCallback(vm.OnResponseNameChange);
+        }
+
+        public static void UnRegisterEventsOnDisable(this  vm)
+        {
+            GameLogicManager.Inst.UnRegisterLevelUpCallback(vm.OnResponseLevelUp);
+            GameLogicManager.Inst.UnRegisterNameChangeCallback(vm.OnResponseNameChange);
+        }
+
+        public static void OnResponseLevelUp(this  vm, int userId, int level)
+        {
+            if (vm.UserId != userId)
+                return;
+
+            vm.Level = level;
+        }
+
+        public static void OnResponseNameChange(this  vm, int userId, string name)
+        {
+            if (vm.UserId != userId)
+                return;
+
+            vm.Name = name;
+        }
     }
 }
